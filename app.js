7227850204f79ae5915a47c920f6b095663355a8
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-10-28 20:23:43 +0100

    removed configure() function

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ var middleware = require('./source/middleware');
 var config = require('./config');
 
 var logger = require('./source/utils/logger');
+var env = process.env.NODE_ENV || 'development';
 
 app.set('port', process.env.PORT || 5000);
+
+app.use(morgan('short'));
 
 app.use(methodOverride());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,41 +27,13 @@ app.use(cookieParser());
 app.use(cors());
 app.use(middleware.errors.logHttpErrors());
 
-configure();
+if (env === 'production') {
+	app.use(compression());
+}
+
 require('./source/api')(app);
 
-var env = process.env.NODE_ENV || 'development';
 
 app.listen(app.get('port'), function () {
 	logger.info('[your-app-name] app listening on port ' + app.get('port') + ' ' + env + ' mongo: ' + config.connection);
 });
-
-
-function configure() {
-	switch(env) {
-		case 'development':
-			app.use(morgan('dev'));
-			app.use(compression());
-		break;
-
-		case 'production':
-			app.use(morgan('tiny'));
-			app.use(compression());
-		break;
-
-		case 'staging':
-			app.use(morgan('short'));
-			app.use(compression());
-		break;
-
-		case 'test':
-			app.use(morgan('combine'));
-			app.use(compression());
-		break;
-
-		default:
-			app.use(morgan('dev'));
-			app.use(compression());
-		break;
-	}
-}
